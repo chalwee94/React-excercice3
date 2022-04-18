45d788e63543f1f770c2c716266e74af4fb48009
patrick yoro <patrick.yoro@yahoo.no>
2022-04-19 00:58:57 +0200

    working on my project

---
+++ server/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const movies = [
+    {
+        title: "The Matrix",
+        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        year: 1999
+    },
+    {
+        title: "The Color Purple",
+        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
+        year: 1985
+    }
+]
+
+app.get("/api/movies", (req, res) => {
+    res.json(movies);
+});
+
+app.post("/api/movies", (req, res) => {
+    const { title, plot, year } = req.body;
+    movies.push({title, plot, year});
+    res.status(200).end();
+})
+
+
+app.use(express.static(path.resolve("..", "client", "dist")));
+app.use((req, res) => {
+    res.sendFile(path.resolve("..", "client", "dist", "index.html"));
+})
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log("Started on http://localhost:" + server.address().port);
+})
+
+
